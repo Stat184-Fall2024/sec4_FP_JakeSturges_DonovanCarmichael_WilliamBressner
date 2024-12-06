94884f42eea7bb31cf099ce2c313d8c2fceeafac
williambressner <wpb5200@psu.edu>
2024-12-06 15:21:16 -0500

    Uploading NFL Graphs R File
    Our code for graphs

---
+++ NFL Graphs.R
@@ -0,0 +1,17 @@
+### Making Tables
+## Case = NFL Team per year
+# Team: x axis
+# Year: y axis
+# Win percentage: In the table entries
+
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(rvest)
+data("")
+ggplot( , aes(x = , y = , color = )) +
+  geom_point() +
+  labs(title = "", 
+       x = "", 
+       y = "") +
+  theme_minimal()
