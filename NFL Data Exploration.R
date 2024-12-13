51acad2f69cc0330b7ab71bc5e9a2f58b9abfdf0
williambressner <wpb5200@psu.edu>
2024-12-13 15:21:58 -0500

    Update NFL Data Exploration.R
    My code to create 3 histograms

--- NFL Data Exploration.R
+++ NFL Data Exploration.R
@@ -1,19 +1,45 @@
-### Making Tables
-## Case = NFL Team per year
-# Team: x axis
-# Year: y axis
-# Win percentage: In the table entries
+### Making 3 Histograms
+# Create histogram for WinPercentage with x as win percentage and y as frequency
+Final_Table %>%
+  ggplot(Final_Table,
+         mapping = aes(
+           x = WinPercentage)) +
+  geom_histogram(binwidth = 0.1, 
+                 fill = "blue", 
+                 color = "black", 
+                 alpha = 0.7)+
+  labs(title = "Histogram of Win Percentage",
+       x = "Win Percentage",
+       y = "Frequency") +
+  theme_bw()
 
-library(ggplot2)
-library(dplyr)
-library(tidyverse)
-library(rvest)
-data("")
-ggplot( , aes(x = , y = , color = )) +
-  geom_point() +
-  labs(title = "", 
-       x = "", 
-       y = "") +
-  theme_minimal()
+# Create histogram for Attendance with x as attendance and y as frequency
+Final_Table %>%
+  ggplot(Final_Table,
+         mapping = aes(
+           x = Attendance)) +
+  geom_histogram(binwidth = 5000, 
+                 fill = "green", 
+                 color = "black", 
+                 alpha = 0.7)+
+  labs(title = "Histogram of Attendance",
+       x = "Attendance",
+       y = "Frequency") +
+  theme_bw()
+
+
+# Create histogram for Population with x as population and y as frequency
+Final_Table %>%
+  ggplot(Final_Table,
+         mapping = aes(
+           x = Population)) +
+  geom_histogram(binwidth = 500000, 
+                 fill = "red", 
+                 color = "black", 
+                 alpha = 0.7)+
+  labs(title = "Histogram of Population",
+       x = "Population",
+       y = "Frequency") +
+  theme_bw()
   
 
