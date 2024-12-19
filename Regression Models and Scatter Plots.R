11ff60d179acf84b8ee276c811f440f9cadf2989
jake-sturges <jhs5678@psu.edu>
2024-12-18 19:36:05 -0500

    improve scatterplots with ggplot

--- Regression Models and Scatter Plots.R
+++ Regression Models and Scatter Plots.R
@@ -4,16 +4,35 @@
 
 library(ggplot2)
 
+ggplot(data = Final_Table, 
+       mapping = aes(x = Population / 1000000,# sets x values to attendance, y values to winPercentage
+                     y = WinPercentage * 100, 
+                     color = Team)) + # sets color to match team
+         geom_point(shape = 19) + # sets shape of the points to a closed circle
+  geom_smooth(method = "lm", se = TRUE, color = "red") + # creates a linear line of best fit for the scatterplot
+  labs(
+    x = "Population(Millions)", # sets labels for the graph
+    y = "Win Percentage",
+    title = "Population vs Win Percentage"
+  )
 
-plot(Population~WinPercentage,data = Final_Table, pch = 16, color = "red")
 
-PopulationRegModel <- lm(Population~WinPercentage, data = Final_Table)
-summary(PopulationRegModel)
-
+PopulationRegModel <- lm(WinPercentage~Population, data = Final_Table) # sets regression model for population vs winpercentage
+summary(PopulationRegModel) # creates summary statistics for the above regression model
 
 
+ggplot(data = Final_Table, 
+       mapping = aes(x = Attendance, # sets x values to attendance, y values to winPercentage
+                     y = WinPercentage * 100,
+                     color = Year)) + # codes each point color by year
+  geom_point(shape = 19) + # sets shape of the points to a closed circle
+  geom_smooth(method = "lm", se = TRUE, color = "red") +# creates a linear line of best fit for the scatterplot
+  labs(
+    x = "Attendance",   # sets labels for the graph
+    y = "Win Percentage",
+    title = "Attendance vs Win Percentage"
+  )
 
-plot(Attendance~WinPercentage,data = Final_Table, pch = 16, color = "blue")
 
-StadiumAttendanceRegModel <- lm(Attendance~WinPercentage, data = Final_Table)
-summary(StadiumAttendanceRegModel)
+StadiumAttendanceRegModel <- lm(WinPercentage~Attendance, data = Final_Table) # sets regression model for population vs winpercentage
+summary(StadiumAttendanceRegModel) # creates summary statistics for the above regression model
