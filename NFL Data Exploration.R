718af1d55b7941031280c6b3193bd0c9a9350321
DonnieC23 <dac6526@psu.edu>
2024-12-18 19:57:41 -0500

    Update NFL Data Exploration.R
    Added plan and some updates to the graphs

--- NFL Data Exploration.R
+++ NFL Data Exploration.R
@@ -1,45 +1,61 @@
-### Making 3 Histograms
-# Create histogram for WinPercentage with x as win percentage and y as frequency
-Final_Table %>%
-  ggplot(Final_Table,
+### Goal - Create simple displays to understand the spread of variables
+###        and the potential relationships between them
+# Plan - 
+# 1) Install necessary packages (ggplot,kable)
+# 2) Create histograms of each variable to see general spread
+## a) use ggplot package and Final_Table previously made
+## b) Win Percentage, Population, and Attendace should be graphed
+# 3) Create basic plots to show potential correlation between the data
+## a) Win Pct vs. Population ()
+## b) Attendance vs. Win Pct ()
+## c) Population vs. Attendance () Checking potential covariance in data
+
+# Step 1) Load Packages
+library(ggplot2)
+library(kableExtra)
+library(tidyverse)
+
+# Step 2) Create Histograms 
+# Create histogram for WinPercentage
+ggplot(
+  data = Final_Table,
          mapping = aes(
-           x = WinPercentage)) +
-  geom_histogram(binwidth = 0.1, 
+           x = WinPercentage)) + # x-axis is win percentage
+  geom_histogram(binwidth = 0.05, #new bar every 5% difference in win pct
                  fill = "blue", 
                  color = "black", 
                  alpha = 0.7)+
-  labs(title = "Histogram of Win Percentage",
+  labs(title = "NFL Win Percentage",
        x = "Win Percentage",
        y = "Frequency") +
   theme_bw()
 
+
 # Create histogram for Attendance with x as attendance and y as frequency
-Final_Table %>%
-  ggplot(Final_Table,
+ggplot( 
+  data = Final_Table,
          mapping = aes(
            x = Attendance)) +
   geom_histogram(binwidth = 5000, 
                  fill = "green", 
                  color = "black", 
                  alpha = 0.7)+
-  labs(title = "Histogram of Attendance",
+  labs(title = "NFL Stadium Attendance",
        x = "Attendance",
        y = "Frequency") +
   theme_bw()
 
 
 # Create histogram for Population with x as population and y as frequency
-Final_Table %>%
-  ggplot(Final_Table,
+ggplot(
+  data = Final_Table,
          mapping = aes(
-           x = Population)) +
-  geom_histogram(binwidth = 500000, 
+           x = Population)) + #Population is the x axis
+  geom_histogram(binwidth = 250000, #For every 250 thousand split bars
                  fill = "red", 
                  color = "black", 
                  alpha = 0.7)+
-  labs(title = "Histogram of Population",
-       x = "Population",
+  labs(title = "NFL City Populations",
+       x = "City Population",
        y = "Frequency") +
   theme_bw()
-  
-
